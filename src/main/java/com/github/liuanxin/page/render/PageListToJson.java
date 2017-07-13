1d8715e0a5cbaf35ab8b7b0fed2ad456f24d00da
liuanxin <liu_anxin@163.com>
2017-07-13 16:13:52 +0800

    fix

---
+++ src/main/java/com/github/liuanxin/page/render/PageListToJson.java
@@ -0,0 +1,38 @@
+package com.github.liuanxin.page.render;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.github.liuanxin.page.model.PageList;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/** 将 PageList 渲染成 json, 使用 Jackson 序列化 */
+public class PageListToJson extends ObjectMapper {
+
+    /**
+     * 将数据返回时, 只输出 总条数 和 当前页的数据.<br/>
+     *
+     * 前端根据 page limit(此两个值由他们传入) 和 总条数 三个值, 输出相关的 1 2 3 及置灰相关的按钮等
+     */
+    public PageListToJson() {
+        super();
+
+        registerModule(new SimpleModule().addSerializer(PageList.class, new JsonSerializer<PageList>() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void serialize(PageList value, JsonGenerator gen, SerializerProvider sp) throws IOException {
+                Map<String, Object> returnMap = new HashMap<String, Object>();
+                returnMap.put("items", new ArrayList(value));
+                returnMap.put("total", value.getCount());
+
+                gen.writeObject(returnMap);
+            }
+        }));
+    }
+}
---
+++ src/main/java/com/github/liuanxin/page/render/PageListToPage.java
@@ -0,0 +1,56 @@
+package com.github.liuanxin.page.render;
+
+import com.github.liuanxin.page.model.PageList;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/** 将 PageList 渲染成 model, 放入 spring mvc 的渲染上下文中, 使用拦截器实现 */
+public class PageListToPage implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        return true;
+    }
+
+    /** 如果在当前上下文中已经放了对象的属性, 则判断有没有 PageList, 有就显式的将总条数也放入上下文 */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            Map<String, Object> newModel = new HashMap<String, Object>();
+            for (Map.Entry<String, Object> item : modelAndView.getModel().entrySet()) {
+                Object value = item.getValue();
+                if (value instanceof PageList) {
+                    newModel.put(item.getKey() + "-count", ((PageList) value).getCount());
+                }
+            }
+            if (!newModel.isEmpty()) {
+                modelAndView.addAllObjects(newModel);
+            }
+        }
+        // 在 model 和 request 中都放
+        Enumeration enumeration = request.getAttributeNames();
+        while (enumeration.hasMoreElements()) {
+            Object element = enumeration.nextElement();
+            if (element instanceof String) {
+                String name = (String) element;
+                Object attribute = request.getAttribute(name);
+                if (attribute instanceof PageList) {
+                    request.setAttribute(name + "-count", ((PageList) attribute).getCount());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+                                Object handler, Exception ex) throws Exception {
+    }
+}
