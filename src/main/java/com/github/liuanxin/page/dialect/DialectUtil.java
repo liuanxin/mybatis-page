b0f05d9be813afc47f725a06119afea5b934b048
liuanxin <liu_anxin@163.com>
2017-12-01 19:24:53 +0800

    Update DialectUtil.java

--- src/main/java/com/github/liuanxin/page/dialect/DialectUtil.java
+++ src/main/java/com/github/liuanxin/page/dialect/DialectUtil.java
@@ -16,16 +16,14 @@ public class DialectUtil {
     private static final Map<String, Class<? extends Dialect>> DIALECT_MAP = new HashMap<String, Class<? extends Dialect>>();
     static {
         DIALECT_MAP.put("h2", H2Dialect.class);
-
         DIALECT_MAP.put("sqlite", MySqlDialect.class);
+
+        DIALECT_MAP.put("postgresql", PostgreSQLDialect.class);
+        DIALECT_MAP.put("oracle", OracleDialect.class);
+        
         DIALECT_MAP.put("mysql", MySqlDialect.class);
         DIALECT_MAP.put("mariadb", MySqlDialect.class);
-
-        DIALECT_MAP.put("oracle", OracleDialect.class);
-        DIALECT_MAP.put("postgresql", PostgreSQLDialect.class);
-
-        DIALECT_MAP.put("sqlite", MySqlDialect.class);
-
+        
         DIALECT_MAP.put("sqlserver", SQLServer2000Dialect.class);
         DIALECT_MAP.put("sqlserver2000", SQLServer2000Dialect.class);
         DIALECT_MAP.put("sqlserver2005", SQLServer2005Dialect.class);
